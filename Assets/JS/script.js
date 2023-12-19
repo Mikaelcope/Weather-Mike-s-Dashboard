4da7555c8dabee391735e0ccca447e28cf5dd326
Mikael <copemikael@gmail.com>
2023-12-19 21:24:08 +0200

    Debugging Javascript

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -22,8 +22,10 @@ searchBtn.on('click', function(event){
     //Input button into search history
         //get data from imput//
         
+
+
         var newData = searchVal
-        console.log(newData)
+        // console.log(newData)
     
             if(localStorage.getItem('data') == null){
                 localStorage.setItem('data', '[]');
@@ -31,15 +33,25 @@ searchBtn.on('click', function(event){
     
             //get old data//
             var oldData = JSON.parse(localStorage.getItem('data'));
+
+            if(!oldData.includes(newData)){
+
             oldData.push(newData);
-    
+            // console.log(oldData)
+            }
             localStorage.setItem('data', JSON.stringify(oldData));
 
+            if(localStorage.getItem('data')!= null){
+                
+                for(j=0; j < oldData.length; j++){
+                imputBtn = $('<button>')
+                imputBtn.addClass('localBTN')
+                // console.log(oldData)
+                imputBtn.text(oldData[j])
+                // console.log(imputBtn)
+                $('#history').append(imputBtn)
+            }
 
-    imputBtn = $('<button>')
-    imputBtn.text(searchVal)
-    console.log(imputBtn)
-    $('#history').append(imputBtn)
     // history.appendChild(imputBtn)
 
     var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchVal + "&appid=69a234fe9433fd65a7b8623a7427cfc3"
@@ -48,7 +60,8 @@ searchBtn.on('click', function(event){
         return response.json();
     })
     .then(function(data){
-        // console.log(data)
+        // if(data != undefined)
+        console.log(data)
         var singleDay = $('<div>')
         var p1 = $('<h2>')
         p1.text(searchVal + '   ' + day)
@@ -62,22 +75,29 @@ searchBtn.on('click', function(event){
         today.append(singleDay)
 
 
-        // var forecastArray = data.list
-        // for(i = 0; i < forecastArray.length; i++){
+        var forecastArray = data.list
+        for(i = 0; i < forecastArray.length; i++){
 
             
-        //     var date = $('<h3>')
-        //     date.text(day)
-        //     var card1 = $('<p>')
-        //     card1.text('icon')
-        //     var card2 = $('<p>')
-        //     card2.text("Temp: " + forecastArray[i].main.temp)
+            var date = $('<h3>')
+            date.text(forecastArray[i].dt_txt)
+            console.log(date)
+            var card1 = $('<p>')
+            card1.text('icon')
+            var card2 = $('<p>')
+            card2.text("Temp: " + forecastArray[i].main.temp)
 
-        //     var card3 = $('<p>')
-        //     card3.text("Wind: " + forecastArray[i].wind.speed)
-        //     var card4 = $('<p>')
-        //     card4.text("Humidity: " + forecastArray[i].main.humidity)
-        //     console.log(card4)
+            var card3 = $('<p>')
+            card3.text("Wind: " + forecastArray[i].wind.speed)
+            var card4 = $('<p>')
+            card4.text("Humidity: " + forecastArray[i].main.humidity)
+            // console.log(card4)
+
+            var card = $('<div>')
+            card.addClass('card1')
+            card.append(date, card1, card2, card3, card4)
+
+            foreCast.append(card)
 
         //     // var foreCast = $('div')
         //     // foreCast.append(card1, card2, card3, card4)
@@ -89,40 +109,11 @@ searchBtn.on('click', function(event){
         //     // classHead.append(foreCast)
         //     //  console.log(forecastArray[i].main.temp)
         //     // console.log(foreCast)
-        // }
+        }
 
-                // console.log(event.target)
-                // console.log(event.target.getAttribute('id'))
-                // console.log(event.target.previousElementSibling.value)
-    
-                // console.log(localStorageData)
-    
-                // if (localStorageData === null) {
-                //     localStorageData = []
-                //     localStorageData.push({
-                //         value: searchVal
-                //     })
-                //   } else {
-                //     localStorageData.push({
-                //         value: searchVal
-                //     })
-                // }
-                
-
-        // }
-    
-        // for (var p = 0; p < localStorageData.length; p++) {
-        //     console.log(localStorageData[p])
-        //     var hour = localStorageData[p].note
-        //     var valueStore = localStorageData[p].value
-        //     var hourBTN = $('#' + hour)
-        //     console.log(hourBTN)
-        //     console.log(hourBTN.siblings()[1])
-        //     hourBTN.siblings()[1].textContent = valueStore
-        // }
     })
    
-})
+}})
 
 
 
