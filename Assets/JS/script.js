12035432438ee021058c93d465fd3b84c8fa650f
Mikael <copemikael@gmail.com>
2023-12-20 13:00:59 +0200

    Debugging

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -6,182 +6,152 @@ var foreCast = $('#fiveDay')
 var day = dayjs().format('DD-MM-YYYY')
 
 
-var oldData = JSON.parse(localStorage.getItem('data'));
-
-localStorage.setItem('data', JSON.stringify(oldData));
-
-if(localStorage.getItem('data')!= null){
+// if(localStorage.getItem('data')!= null){
     
-    for(j=0; j < oldData.length; j++){
-    imputBtn = $('<button>')
-    imputBtn.addClass('localBTN')
-    // console.log(oldData)
-    imputBtn.text(oldData[j])
-    // console.log(imputBtn)
-    $('#history').append(imputBtn)
-}}
+//     for(j=0; j < oldData.length; j++){
+//     imputBtn = $('<button>')
+//     imputBtn.addClass('localBTN')
+//     // console.log(oldData)
+//     imputBtn.text(oldData[j])
+//     // console.log(imputBtn)
+//     $('#history').append(imputBtn)
+// }}
 
+// var storageBTN = $('.localBTN')
+// storageBTN.on('click', function(event){
+//     event.preventDefault()
+//     console.log('this')
+//     searchVal = $('this').val()
+//     console.log(searchVal)
 
-searchBtn.on('click', function (event) {
+//     var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchVal + "&appid=69a234fe9433fd65a7b8623a7427cfc3"
+//     fetch(queryURL)
+//     .then(function (response){
+//     return response.json();
+//     })
+//     .then(function(data){
+//     // if(data != undefined)
+//     console.log(data)
+//     var singleDay = $('<div>')
+//     var p1 = $('<h2>')
+//     var iconSin = $('<img>')
+//     iconSin.attr('src', 'https://openweathermap.org/img/w/' + data.list[0].weather[0].icon + '.png')
+//     console.log(iconSin)
+
+//     p1.text(searchVal + '  ' + day)
+//     p1.append(iconSin)
+//     var p2 = $('<p>')
+//     p2.text("Temp: " + data.list[0].main.temp)
+//     var p3 = $('<p>')
+//     p3.text("Wind: " + data.list[0].wind.speed)
+//     var p4 = $('<p>')
+//     p4.text("Humidity: " + data.list[0].main.humidity)
+//     singleDay.append(p1, p2, p3, p4)
+//     today.append(singleDay)
+
+//     singleDay.addClass('singleBox')
+
+//     })
+
+// })
+
+searchBtn.on('click', function(event){
     event.preventDefault()
     today.empty()
     $('#history').empty()
     var searchVal = searchImput.val().trim()
 
+        console.log(searchVal)
+    
 
-    var newData = searchVal
-    // console.log(newData)
 
-    if (localStorage.getItem('data') == null) {
-        localStorage.setItem('data', '[]');
-    }
+        var newData = searchVal
+        console.log(newData)
+    
 
-    //get old data//
-    var oldData = JSON.parse(localStorage.getItem('data'));
+    
+            //get old data//
+            if(localStorage.getItem('data') == null){
+                localStorage.setItem('data', '[]');
+            }
+            var oldData = JSON.parse(localStorage.getItem('data'));
+            console.log(oldData)
+            if(!oldData.includes(newData)){
 
-    if (!oldData.includes(newData)) {
+            oldData.push(newData);
+            console.log(oldData)
+            }
+            localStorage.setItem('data', JSON.stringify(oldData));
 
-        oldData.push(newData);
-        // console.log(oldData)
-    }
-    localStorage.setItem('data', JSON.stringify(oldData));
-
-    if (localStorage.getItem('data') != null) {
-
-        for (j = 0; j < oldData.length; j++) {
-            imputBtn = $('<button>')
-            imputBtn.addClass('localBTN')
-            // console.log(oldData)
-            imputBtn.text(oldData[j])
-            // console.log(imputBtn)
-            $('#history').append(imputBtn)
-        }
+            if(localStorage.getItem('data')!= null){
+                
+                for(j=0; j < oldData.length; j++){
+                imputBtn = $('<button>')
+                imputBtn.addClass('localBTN')
+                // console.log(oldData)
+                imputBtn.text(oldData[j])
+                // console.log(imputBtn)
+                $('#history').append(imputBtn)
+            }
 
 
         var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchVal + "&appid=69a234fe9433fd65a7b8623a7427cfc3"
-        fetch(queryURL)
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (data) {
-                // if(data != undefined)
-                // console.log(data)
-                var singleDay = $('<div>')
-                var p1 = $('<h2>')
-                var iconSin = $('<img>')
-                iconSin.attr('src', 'https://openweathermap.org/img/w/' + data.list[0].weather[0].icon + '.png')
-                // console.log(iconSin)
-
-                p1.text(searchVal + '  ' + day)
-                p1.append(iconSin)
-                var p2 = $('<p>')
-                p2.text("Temp: " + data.list[0].main.temp)
-                var p3 = $('<p>')
-                p3.text("Wind: " + data.list[0].wind.speed)
-                var p4 = $('<p>')
-                p4.text("Humidity: " + data.list[0].main.humidity)
-                singleDay.append(p1, p2, p3, p4)
-                today.append(singleDay)
-
-                singleDay.addClass('singleBox')
-
-
-                var forecastArray = data.list
-                for (i = 0; i < forecastArray.length; i++) {
-
-
-                    var date = $('<h3>')
-                    date.text(forecastArray[i].dt_txt)
-                    // console.log(date)
-                    var card1 = $('<img>')
-                    card1.attr('src', 'https://openweathermap.org/img/w/' + data.list[i].weather[0].icon + '.png')
-                    var card2 = $('<p>')
-                    card2.text("Temp: " + forecastArray[i].main.temp)
-
-                    var card3 = $('<p>')
-                    card3.text("Wind: " + forecastArray[i].wind.speed)
-                    var card4 = $('<p>')
-                    card4.text("Humidity: " + forecastArray[i].main.humidity)
-                    // console.log(card4)
-
-                    var card = $('<div>')
-                    card.addClass('card1')
-                    card.append(date, card1, card2, card3, card4)
-
-                    foreCast.append(card)
-
-                    var foreHead = $('#foreHead')
-                    foreHead.text('5-Day 3 Hour Forecast:')
-
-                }
-
-            })
-
-    }
-})
-
-var storageBTN = $('.localBTN')
-storageBTN.on('click', function (event) {
-    console.log('click')
-    event.preventDefault()
-    today.empty()
-    console.log(today)
-    searchVal = event.target.innerHTML
-    console.log(searchVal)
-
-    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchVal + "&appid=69a234fe9433fd65a7b8623a7427cfc3"
-    fetch(queryURL)
-        .then(function (response) {
-            return response.json();
+            fetch(queryURL)
+         .then(function (response){
+        return response.json();
         })
-        .then(function (data) {
-            // console.log(data)
-            var singleDay = $('<div>')
-            var p1 = $('<h2>')
-            var iconSin = $('<img>')
-            iconSin.attr('src', 'https://openweathermap.org/img/w/' + data.list[0].weather[0].icon + '.png')
-            // console.log(iconSin)
+        .then(function(data){
+        // if(data != undefined)
+        console.log(data)
+        var singleDay = $('<div>')
+        var p1 = $('<h2>')
+        var iconSin = $('<img>')
+        iconSin.attr('src', 'https://openweathermap.org/img/w/' + data.list[0].weather[0].icon + '.png')
+        console.log(iconSin)
+        
+        p1.text(searchVal + '  ' + day)
+        p1.append(iconSin)
+        var p2 = $('<p>')
+        p2.text("Temp: " + data.list[0].main.temp)
+        var p3 = $('<p>')
+        p3.text("Wind: " + data.list[0].wind.speed)
+        var p4 = $('<p>')
+        p4.text("Humidity: " + data.list[0].main.humidity)
+        singleDay.append(p1, p2, p3, p4)
+        today.append(singleDay)
 
-            p1.text(searchVal + '  ' + day)
-            p1.append(iconSin)
-            var p2 = $('<p>')
-            p2.text("Temp: " + data.list[0].main.temp)
-            var p3 = $('<p>')
-            p3.text("Wind: " + data.list[0].wind.speed)
-            var p4 = $('<p>')
-            p4.text("Humidity: " + data.list[0].main.humidity)
-            singleDay.append(p1, p2, p3, p4)
-            today.append(singleDay)
+        singleDay.addClass('singleBox')
+    
 
-            singleDay.addClass('singleBox')
+        var forecastArray = data.list
+        for(i = 0; i < forecastArray.length; i++){
 
-            var forecastArray = data.list
-            for (i = 0; i < forecastArray.length; i++) {
+            
+            var date = $('<h3>')
+            date.text(forecastArray[i].dt_txt)
+            // console.log(date)
+            var card1 = $('<img>')
+            card1.attr('src', 'https://openweathermap.org/img/w/' + data.list[i].weather[0].icon + '.png')
+            var card2 = $('<p>')
+            card2.text("Temp: " + forecastArray[i].main.temp)
 
+            var card3 = $('<p>')
+            card3.text("Wind: " + forecastArray[i].wind.speed)
+            var card4 = $('<p>')
+            card4.text("Humidity: " + forecastArray[i].main.humidity)
+            // console.log(card4)
 
-                var date = $('<h3>')
-                date.text(forecastArray[i].dt_txt)
-                // console.log(date)
-                var card1 = $('<img>')
-                card1.attr('src', 'https://openweathermap.org/img/w/' + data.list[i].weather[0].icon + '.png')
-                var card2 = $('<p>')
-                card2.text("Temp: " + forecastArray[i].main.temp)
+            var card = $('<div>')
+            card.addClass('card1')
+            card.append(date, card1, card2, card3, card4)
 
-                var card3 = $('<p>')
-                card3.text("Wind: " + forecastArray[i].wind.speed)
-                var card4 = $('<p>')
-                card4.text("Humidity: " + forecastArray[i].main.humidity)
-                // console.log(card4)
+            foreCast.append(card)
 
-                var card = $('<div>')
-                card.addClass('card1')
-                card.append(date, card1, card2, card3, card4)
+            var foreHead = $('#foreHead')
+            foreHead.text('5-Day 3 Hour Forecast:')
 
-                foreCast.append(card)
+        }
 
-                var foreHead = $('#foreHead')
-                foreHead.text('5-Day 3 Hour Forecast:')
-
-            }
-        })
+    })
+   }
 })
