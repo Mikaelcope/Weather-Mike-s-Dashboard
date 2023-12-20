d67599450d45d58d7e2b9735b2137d9ada3056f5
Mikael <copemikael@gmail.com>
2023-12-20 11:38:23 +0200

    Changes to Javascript and README

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -6,12 +6,6 @@ var foreCast = $('#fiveDay')
 var day = dayjs().format('DD-MM-YYYY')
 
 
-// if(localStorage.getItem('data'!=null){
-//     newBTN = $('<button>')
-//     newBTN.text(localStorage)
-// })
-
-
 var oldData = JSON.parse(localStorage.getItem('data'));
 
 localStorage.setItem('data', JSON.stringify(oldData));
@@ -27,156 +21,167 @@ if(localStorage.getItem('data')!= null){
     $('#history').append(imputBtn)
 }}
 
-searchBtn.on('click', function(event){
+
+searchBtn.on('click', function (event) {
     event.preventDefault()
     today.empty()
     $('#history').empty()
     var searchVal = searchImput.val().trim()
 
-    //Input button into search history
-        //get data from imput//
-        
 
+    var newData = searchVal
+    // console.log(newData)
 
-        var newData = searchVal
-        // console.log(newData)
-    
-            if(localStorage.getItem('data') == null){
-                localStorage.setItem('data', '[]');
-            }
-    
-            //get old data//
-            var oldData = JSON.parse(localStorage.getItem('data'));
+    if (localStorage.getItem('data') == null) {
+        localStorage.setItem('data', '[]');
+    }
 
-            if(!oldData.includes(newData)){
+    //get old data//
+    var oldData = JSON.parse(localStorage.getItem('data'));
 
-            oldData.push(newData);
+    if (!oldData.includes(newData)) {
+
+        oldData.push(newData);
+        // console.log(oldData)
+    }
+    localStorage.setItem('data', JSON.stringify(oldData));
+
+    if (localStorage.getItem('data') != null) {
+
+        for (j = 0; j < oldData.length; j++) {
+            imputBtn = $('<button>')
+            imputBtn.addClass('localBTN')
             // console.log(oldData)
-            }
-            localStorage.setItem('data', JSON.stringify(oldData));
+            imputBtn.text(oldData[j])
+            // console.log(imputBtn)
+            $('#history').append(imputBtn)
+        }
 
-            if(localStorage.getItem('data')!= null){
-                
-                for(j=0; j < oldData.length; j++){
-                imputBtn = $('<button>')
-                imputBtn.addClass('localBTN')
-                // console.log(oldData)
-                imputBtn.text(oldData[j])
-                // console.log(imputBtn)
-                $('#history').append(imputBtn)
-            }
 
-    // history.appendChild(imputBtn)
+        var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchVal + "&appid=69a234fe9433fd65a7b8623a7427cfc3"
+        fetch(queryURL)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                // if(data != undefined)
+                // console.log(data)
+                var singleDay = $('<div>')
+                var p1 = $('<h2>')
+                var iconSin = $('<img>')
+                iconSin.attr('src', 'https://openweathermap.org/img/w/' + data.list[0].weather[0].icon + '.png')
+                // console.log(iconSin)
+
+                p1.text(searchVal + '  ' + day)
+                p1.append(iconSin)
+                var p2 = $('<p>')
+                p2.text("Temp: " + data.list[0].main.temp)
+                var p3 = $('<p>')
+                p3.text("Wind: " + data.list[0].wind.speed)
+                var p4 = $('<p>')
+                p4.text("Humidity: " + data.list[0].main.humidity)
+                singleDay.append(p1, p2, p3, p4)
+                today.append(singleDay)
+
+                singleDay.addClass('singleBox')
+
+
+                var forecastArray = data.list
+                for (i = 0; i < forecastArray.length; i++) {
+
+
+                    var date = $('<h3>')
+                    date.text(forecastArray[i].dt_txt)
+                    // console.log(date)
+                    var card1 = $('<img>')
+                    card1.attr('src', 'https://openweathermap.org/img/w/' + data.list[i].weather[0].icon + '.png')
+                    var card2 = $('<p>')
+                    card2.text("Temp: " + forecastArray[i].main.temp)
+
+                    var card3 = $('<p>')
+                    card3.text("Wind: " + forecastArray[i].wind.speed)
+                    var card4 = $('<p>')
+                    card4.text("Humidity: " + forecastArray[i].main.humidity)
+                    // console.log(card4)
+
+                    var card = $('<div>')
+                    card.addClass('card1')
+                    card.append(date, card1, card2, card3, card4)
+
+                    foreCast.append(card)
+
+                    var foreHead = $('#foreHead')
+                    foreHead.text('5-Day 3 Hour Forecast:')
+
+                }
+
+            })
+
+    }
+})
+
+var storageBTN = $('.localBTN')
+storageBTN.on('click', function (event) {
+    console.log('click')
+    event.preventDefault()
+    today.empty()
+    console.log(today)
+    searchVal = event.target.innerHTML
+    console.log(searchVal)
 
     var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchVal + "&appid=69a234fe9433fd65a7b8623a7427cfc3"
     fetch(queryURL)
-    .then(function (response){
-        return response.json();
-    })
-    .then(function(data){
-        // if(data != undefined)
-        console.log(data)
-        var singleDay = $('<div>')
-        var p1 = $('<h2>')
-        p1.text(searchVal + '   ' + day)
-        var p2 = $('<p>')
-        p2.text("Temp: " + data.list[0].main.temp)
-        var p3 = $('<p>')
-        p3.text("Wind: " + data.list[0].wind.speed)
-        var p4 = $('<p>')
-        p4.text("Humidity: " + data.list[0].main.humidity)
-        singleDay.append(p1, p2, p3, p4)
-        today.append(singleDay)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            // console.log(data)
+            var singleDay = $('<div>')
+            var p1 = $('<h2>')
+            var iconSin = $('<img>')
+            iconSin.attr('src', 'https://openweathermap.org/img/w/' + data.list[0].weather[0].icon + '.png')
+            // console.log(iconSin)
+
+            p1.text(searchVal + '  ' + day)
+            p1.append(iconSin)
+            var p2 = $('<p>')
+            p2.text("Temp: " + data.list[0].main.temp)
+            var p3 = $('<p>')
+            p3.text("Wind: " + data.list[0].wind.speed)
+            var p4 = $('<p>')
+            p4.text("Humidity: " + data.list[0].main.humidity)
+            singleDay.append(p1, p2, p3, p4)
+            today.append(singleDay)
+
+            singleDay.addClass('singleBox')
+
+            var forecastArray = data.list
+            for (i = 0; i < forecastArray.length; i++) {
 
 
-        var forecastArray = data.list
-        for(i = 0; i < forecastArray.length; i++){
+                var date = $('<h3>')
+                date.text(forecastArray[i].dt_txt)
+                // console.log(date)
+                var card1 = $('<img>')
+                card1.attr('src', 'https://openweathermap.org/img/w/' + data.list[i].weather[0].icon + '.png')
+                var card2 = $('<p>')
+                card2.text("Temp: " + forecastArray[i].main.temp)
 
-            
-            var date = $('<h3>')
-            date.text(forecastArray[i].dt_txt)
-            console.log(date)
-            var card1 = $('<p>')
-            card1.text('icon')
-            var card2 = $('<p>')
-            card2.text("Temp: " + forecastArray[i].main.temp)
+                var card3 = $('<p>')
+                card3.text("Wind: " + forecastArray[i].wind.speed)
+                var card4 = $('<p>')
+                card4.text("Humidity: " + forecastArray[i].main.humidity)
+                // console.log(card4)
 
-            var card3 = $('<p>')
-            card3.text("Wind: " + forecastArray[i].wind.speed)
-            var card4 = $('<p>')
-            card4.text("Humidity: " + forecastArray[i].main.humidity)
-            // console.log(card4)
+                var card = $('<div>')
+                card.addClass('card1')
+                card.append(date, card1, card2, card3, card4)
 
-            var card = $('<div>')
-            card.addClass('card1')
-            card.append(date, card1, card2, card3, card4)
+                foreCast.append(card)
 
-            foreCast.append(card)
+                var foreHead = $('#foreHead')
+                foreHead.text('5-Day 3 Hour Forecast:')
 
-        //     // var foreCast = $('div')
-        //     // foreCast.append(card1, card2, card3, card4)
-        //     // var foreCast = $('<div>')
-        //     // foreCast.append(card)
-        //     // var foreCastHTML = $('#fiveDay')
-        //     // foreCastHTML.append(foreCast)
-        
-        //     // classHead.append(foreCast)
-        //     //  console.log(forecastArray[i].main.temp)
-        //     // console.log(foreCast)
-        }
-
-    })
-   
-}})
-
-
-
-
-    
-
-// 1. When user search for a city in the input, call weather API and show the result in the HTML
-//    - Add event listener to form submit
-//    - Get the user input value
-//    - Build the API query URL based on the user input value
-//    - Call the API and render the result in the HTML
-//        - Get the city name and show it in the main weather forecast card
-//        - Get the first weather forecast item and get the following values
-//            - date
-//            - temperature
-//            - wind speed
-//            - humidity
-//            - icon
-//        - render those values to the main card
-//        - Loop through all weathers array and get the following values
-//            - date
-//            - temperature
-//            - wind speed
-//            - humidity
-//            - icon
-//        - render those values to the smaller card
-// 2. When user search for a city, store it in local storage
-// 3. On initial page load load the search history and show it as a list in the HTML
-//    - ....
-//    - Build the API query URL based on the history stored in local storage
-//    - Call the API and render the result in the HTML
-// 4. When user click on the search history, call weather API and show the result in the HTML
-// 5. CSS
-
-// fetch('https://api.openweathermap.org/data/2.5/forecast?q=london&appid=[api-key]&units=metric')
-//     .then(function(response) {
-//         return response.json();
-//     })
-//     .then(function(data) {
-//         console.log(data);
-    // })
-
-
-
-        // var query1Day = "https://api.openweathermap.org/data/2.5/weather?q=" + searchVal + "&appid=69a234fe9433fd65a7b8623a7427cfc3"
-    
-    
-//     fetch(query1Day)
-//     .then(function (response){
-//         return response.json();
-//     })
-//     .then(function(data){
+            }
+        })
+})
--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # Weather-Mike-s-Dashboard
 A dashboard displaying weather for the next 5 days in your selected city.
+
+## Description
+I have created a URL that has a weather report function based off of an input city. Users insert a city of choice and click search. This generates the weather report for the day as well as a forcast for the following 5 days. It extracts this information using API for the following website: https://openweathermap.org/ .
+
+### Installation
+Click on the following link to access the URL:
+https://mikaelcope.github.io/Weather-Mike-s-Dashboard/
+
+
+### Usage
+Once the URL has loaded this is what you will see: 
+![alt text](./Assets/Images/Screenshot%202023-12-12%20194752.png)<br>
+Note: Image may display multiple colours based on the time of day. <br>
+Grey: Passed Hours <br>
+Red: Current Hour <br>
+Green: Future Hours (Available Hours) <br>
+
+The page will load any information in the current local storage, if none, inputs will be blank. The user will be able to input tasks planned for the day and save. This information will be saved to the local storage. The user can then delete the information in the text area and save to blank the space.
+
+
+### Credits
+EdX front end web development course.
+
+
+### License
+MIT License
+
+Copyright (c) [2023] [Mikael Cope]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
